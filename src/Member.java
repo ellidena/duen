1a658a716af81a7faefc81ef5049f8a93f69fb07
Daniella <ellidena@gmail.com>
2025-11-27 17:28:14 +0100

    added method to calculate age of member

--- src/Member.java
+++ src/Member.java
@@ -1,8 +1,10 @@
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Member {
     private String name;
     private LocalDate birthDate;
+    private int age;
     private boolean isActive;
     private boolean hasPaid;
 
@@ -21,8 +23,28 @@ public class Member {
         return birthDate;
     }
 
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public boolean isHasPaid() {
+        return hasPaid;
+    }
+
+    public boolean isCompetitor() {
+        return isCompetitor;
+    }
+
+    public int calculateAge(){
+        LocalDate currentDate = LocalDate.now();
+        Period period = Period.between(birthDate, currentDate);
+        return period.getYears();
+    }
+
     @Override
     public String toString(){
         return getBirthDate()+" "+getName();
     }
+
+
 }
