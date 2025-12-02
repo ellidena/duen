d556c38040edbcdb53533cace5fdad1aa2b1674b
Daniella <ellidena@gmail.com>
2025-12-02 21:31:05 +0100

    minor changes

--- src/UI/MainMenu.java
+++ src/UI/MainMenu.java
@@ -9,7 +9,7 @@ public class MainMenu {
     // printing menus and taking input
     public static void main(String[] args) {
         MemberController memberController = new MemberController();
-        //MemberFileHandler memberFileHandler = new MemberFileHandler("TuesDayTest.txt"); // TODO is this even needed?? REMOVE!?
+
         Member member1 = new Member("dani","norgren", LocalDate.of(1990,10,19),false,true,true);
 
         memberController.addNewMember("testFirstName","testSurName",LocalDate.of(1988,05,21),false,true,false);
